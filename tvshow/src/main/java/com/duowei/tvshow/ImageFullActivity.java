778e55d6f50a9f1cdbb1882b66dcec8df0df3966
cyy <276523847@qq.com>
2017-04-21 16:35:47 +0800

    tvshow04215

--- tvshow/src/main/java/com/duowei/tvshow/ImageFullActivity.java
+++ tvshow/src/main/java/com/duowei/tvshow/ImageFullActivity.java
@@ -1,16 +1,33 @@
 package com.duowei.tvshow;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.duowei.tvshow.bean.KDSCall;
+import com.duowei.tvshow.contact.Consts;
+import com.duowei.tvshow.dialog.CallDialog;
+import com.duowei.tvshow.event.CallEvent;
+import com.duowei.tvshow.httputils.Post6;
+import com.duowei.tvshow.httputils.Post7;
 import com.duowei.tvshow.view.RecyclerBanner;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechSynthesizer;
+
+import org.greenrobot.eventbus.Subscribe;
 
 import java.util.ArrayList;
 
-public class ImageFullActivity extends AppCompatActivity {
+import de.greenrobot.event.EventBus;
 
+public class ImageFullActivity extends AppCompatActivity {
+    // 语音合成对象
+    private SpeechSynthesizer mTts;
+    private CallDialog mCallDialog;
+    private Handler mHandler;
+    private Runnable mRun;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +40,90 @@ public class ImageFullActivity extends AppCompatActivity {
         ArrayList<String> imgPaths = intent.getStringArrayListExtra("selectPaths");
         RecyclerBanner rb = (RecyclerBanner) findViewById(R.id.recyclebanner);
         rb.setDatas(imgPaths);
+
+        EventBus.getDefault().register(this);
+        //开启呼叫轮询
+        startCall();
+        // 初始化合成对象
+        mTts = SpeechSynthesizer.createSynthesizer(ImageFullActivity.this, null);
+        //初呼叫界面
+        mCallDialog = CallDialog.getInstance();
+    }
+    /**呼叫轮询*/
+    private synchronized void startCall() {
+        //呼叫显示时间
+        if(Consts.callTime<=0){
+            return;
+        }
+        if(mHandler!=null){
+            mHandler.removeCallbacks(mRun);
+        }
+        mHandler = new Handler();
+        mHandler.postDelayed(mRun=new Runnable() {
+            @Override
+            public void run() {
+                mHandler.postDelayed(this,1000);
+                Post6.instance().getCall();
+            }
+        },5000);
+    }
+    @Subscribe
+    public void onEvent(final CallEvent event){
+        final KDSCall call = event.call;
+        //停止轮询
+        mHandler.removeCallbacks(mRun);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (call !=null){
+                    //暂停视频
+                    /**显示呼叫界面*/
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //显示
+                            mCallDialog.callShow(ImageFullActivity.this, call.getTableno());
+                            /**播音*/
+                            //设置合成语速
+                            mTts.setParameter(SpeechConstant.SPEED, "20");
+                            //设置合成音调
+                            mTts.setParameter(SpeechConstant.PITCH, "50");
+                            //设置合成音量
+                            mTts.setParameter(SpeechConstant.VOLUME, "100");
+                            int code = mTts.startSpeaking("请"+call.getTableno()+"号到柜台取餐", null);
+                        }
+                    });
+                    /**删除服务器上这条记录*/
+                    String xh = call.getXh();
+                    String sql="delete from KDSCall where xh='"+xh+"'|";
+                    /**呼叫显示时长*/
+                    try {
+                        Thread.sleep(Consts.callTime*1000);
+                        //继续轮询
+                        mHandler.postDelayed(mRun,1000);
+                        //播报完毕删除本条记录
+                        Post7.Instance().updateCall(sql);
+                        mCallDialog.cancel();
+                        //继续视频播放
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mHandler!=null){
+            mHandler.removeCallbacks(mRun);
+        }
     }
 }
