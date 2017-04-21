b0f9150c9a2ad4539d39284821276a346e2c0e22
cyy <276523847@qq.com>
2017-04-21 16:22:27 +0800

    tvshow04212

--- tvshow/src/main/java/com/duowei/tvshow/VideoFullActivity.java
+++ tvshow/src/main/java/com/duowei/tvshow/VideoFullActivity.java
@@ -1,11 +1,9 @@
 package com.duowei.tvshow;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Toast;
 
 
--- tvshow/src/main/java/com/duowei/tvshow/dialog/CallDialog.java
+++ tvshow/src/main/java/com/duowei/tvshow/dialog/CallDialog.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,19 +29,16 @@ public class CallDialog {
         }
         return dialog;
     }
-    private  LinearLayout mLayout;
+    private  RelativeLayout mLayout;
     private AlertDialog mDialog;
     public synchronized void callShow(Context context,String msg){
         if(mDialog!=null){
             mDialog.dismiss();
         }
-
-
-
         mDialog = new AlertDialog.Builder(context).create();
         //必须先setView，否则在dialog\popuwindow中无法自动弹出软健盘
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mLayout = (LinearLayout)inflater.inflate(R.layout.dialog_item, null);
+        mLayout = (RelativeLayout)inflater.inflate(R.layout.dialog_item, null);
         mDialog.show();
         Window window = mDialog.getWindow();
         window.setContentView(R.layout.dialog_item);
@@ -52,6 +51,8 @@ public class CallDialog {
 
         TextView tvMsg = (TextView) window.findViewById(R.id.tv_msg);
         tvMsg.setText("请"+msg+"号");
+        Animation animation = AnimationUtils.loadAnimation(context, R.anim.scale_call);
+        tvMsg.startAnimation(animation);
     }
     public void cancel(){
         mDialog.dismiss();
